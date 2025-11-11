062ab1f573ebcceb16b4470fc836446d99e1fc92
Nicolás Ruiz <40731287@comunidadort.edu.ar>
2025-11-11 20:48:30 -0300

    chore: Agrega el modelo Car.js a la rama develop antes de fusionar.

---
+++ models/Car.js
@@ -0,0 +1,53 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Car extends Model {
+    static associate(models) {
+      // configurar despues asociaciones
+    }
+  }
+  Car.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    brand: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: true 
+    },
+    description: { // ✨ CAMPO AÑADIDO
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    isRented: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+    },
+    availableUntil: { 
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Car',
+    tableName: 'Cars',
+  });
+  return Car;
+};
