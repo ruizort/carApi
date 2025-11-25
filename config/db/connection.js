c6f3749b823ca439f35c223fa4dfaa10a5922401
Nicolás Ruiz <40731287@comunidadort.edu.ar>
2025-11-25 20:47:29 -0300

    ejecucion de seed

--- config/db/connection.js
+++ config/db/connection.js
@@ -1,6 +1,8 @@
 // src/db/connection.js
 const db = require('../../models'); // Importa los modelos y la instancia de Sequelize
 
+const userFixSeeder = require('../../seeders/20251119175549-fix-users-passwords-for-production');
+
 /**
  * Función que autentica la conexión a la base de datos y sincroniza los modelos.
  */
@@ -14,6 +16,9 @@ async function connectAndSyncDB() {
     await db.sequelize.sync({ alter: true }); 
     console.log('✅ Tablas sincronizadas (Users, Cars, etc.).');
     
+        await userFixSeeder.up(db.sequelize.getQueryInterface(), db.Sequelize);
+      console.log('🌱 Seeder ejecutado: fix-users-passwords-for-production');
+
     // Devolvemos la instancia de Express para que se pueda arrancar
     return db.sequelize; 
 
